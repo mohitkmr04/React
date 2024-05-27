22db27bd547d233e1f6fac3db5d76813494ce3f0
Mohit Kumar <kmrmohit04@gmail.com>
2024-05-27 23:52:07 +0530

    Files ADDED

---
+++ 02counter/src/App.jsx
@@ -0,0 +1,40 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+  const [counter, setCounter] = useState(true)
+  // let counter = 15
+
+  const addValue = () => {
+    //Will be updAted once only
+    // setCounter(counter + 1)
+    // setCounter(counter + 1)
+    // setCounter(counter + 1)
+    // setCounter(counter + 1)
+
+    setCounter((prevCounter) => prevCounter + 1)
+    setCounter((prevCounter) => prevCounter + 1)
+    setCounter((prevCounter) => prevCounter + 1)
+    setCounter((prevCounter) => prevCounter + 1)
+  }
+  const removeValue = () => {
+    setCounter(counter-1)
+  }
+
+  return (
+    <>
+      <h1>React Course with Hitesh {counter}</h1>
+      <h2>Counter Value : {counter}</h2>
+      <button
+      onClick={addValue}
+      >Add value</button> {" "}
+      <button
+      onClick={removeValue}
+      >Remove Value</button>
+      <p>footer: {counter}</p>
+    </>
+  )
+}
+
+export default App
